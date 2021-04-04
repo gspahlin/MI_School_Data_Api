6aeeca3f18fc0ad27f4ff02143e957687f62c7b3
gspahlin <gspahlin@gmail.com>
2021-04-04 00:05:35 -0400

    app now features a route for school data

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ majority_minority = prune_df['majority_minority'].to_numpy()
 #define an empty list for 
 dist_data = []
 
+
+#this for loop packs each row into a dictionary and adds it to a list
 for d in range(len(district)):
     #initialize the dictionary "entry"
     entry = {}
@@ -58,15 +60,59 @@ for d in range(len(district)):
     
     #append the entry into the dist_data list
     dist_data.append(entry)
-    
 
 dist_data_dict = {'district_data': dist_data}
+#do a similar process for the school data
+
+school_df = pd.read_csv('School_data.csv')  
+
+#convert columns to numpy arrays for speed and ease of iteration
+school = school_df['school'].to_numpy()
+s_district = school_df['district'].to_numpy()
+s_lat = school_df['lat'].to_numpy()
+s_lng = school_df['lng'].to_numpy()
+enrollment = school_df['enrollment'].to_numpy()
+prcnt_wht = school_df['percent_white'].to_numpy()
+prcnt_hisp = school_df['percent_hispanic'].to_numpy()
+prcnt_bk = school_df['percent_black'].to_numpy()
+prcnt_asn = school_df['percent_asian'].to_numpy()
+prcnt_othr = school_df['percent_other'].to_numpy()
+s_prcnt_stdnts_eligible_for_frl  = school_df['percent_student_body_eligible_for_frl'].to_numpy()
+s_majority_minority = school_df['majority_minority'].to_numpy()
+
+#variable for school data list
+
+school_data = []
+
+#same for loop logic as the last time
+
+for s in range(len(school)):
+    oed = {}
+    oed.update({'school': school[s]})
+    oed.update({'district' : s_district[s]})
+    oed.update({'lat': s_lat[s]})
+    oed.update({'lng' : s_lng[s]})
+    oed.update({'enrollment' : int(enrollment[s])})
+    oed.update({'prcnt_wht' : int(prcnt_wht[s])})
+    oed.update({'prcnt_hisp' : int(prcnt_hisp[s])})
+    oed.update({'prcnt_bk' : int(prcnt_bk[s])})
+    oed.update({'prcnt_asn' : int(prcnt_asn[s])})
+    oed.update({'prcnt_othr' : int(prcnt_othr[s])})
+    oed.update({'prcnt_stdnts_eligible_for_frl' : int(s_prcnt_stdnts_eligible_for_frl[s])})
+    oed.update({'majority_minority' : s_majority_minority[s]})
+    school_data.append(oed)
+
+#dict for jsonify output
+school_data_dict = {'school_data' : school_data}
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return('Data at /api')
+    return('School data at /school_api, district data at /api')
 
 @app.route("/api", methods=["GET"])
 def api():
@@ -75,6 +121,14 @@ def api():
     response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
+
+@app.route("/school_api", methods=["GET"])
+def school_api():
+    response1 = jsonify(school_data_dict)
+    # Enable Access-Control-Allow-Origin
+    response1.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response1
 
 if __name__ == '__main__':
     app.run(debug = True)
